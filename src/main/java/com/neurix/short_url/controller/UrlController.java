0ae3c5129c77ca8005b08126cb50e71b22d71112
Mohammad Aman <102747651+AmanBits@users.noreply.github.com>
2026-02-23 10:01:14 +0530

    Update UrlController.java

--- src/main/java/com/neurix/short_url/controller/UrlController.java
+++ src/main/java/com/neurix/short_url/controller/UrlController.java
@@ -38,7 +38,6 @@ public class UrlController {
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY) // 301
                 .header(HttpHeaders.LOCATION, longUrl)
                 .header(HttpHeaders.CACHE_CONTROL, "public, max-age=86400")
-                .header(HttpHeaders.VARY, "")
                 .build();
     }
 
