acf513842664451ca46c231d68dff7137266065a
Mohammad Aman <102747651+AmanBits@users.noreply.github.com>
2026-02-23 13:25:30 +0530

    Update RedisConfig.java

--- src/main/java/com/neurix/short_url/config/RedisConfig.java
+++ src/main/java/com/neurix/short_url/config/RedisConfig.java
@@ -1,28 +1,53 @@
 package com.neurix.short_url.config;
 
-import org.springframework.cache.annotation.EnableCaching;
+import io.lettuce.core.resource.ClientResources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.ObjectMapper;
-import tools.jackson.databind.json.JsonMapper;
 
+import java.net.URI;
 import java.time.Duration;
-
 
 @Configuration
 public class RedisConfig {
 
     @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+
+        String redisUrl = System.getenv("REDIS_URL");
+        if (redisUrl == null || redisUrl.isEmpty()) {
+            throw new IllegalStateException("REDIS_URL not set");
+        }
+
+        URI uri = URI.create(redisUrl);
+
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration();
+        config.setHostName(uri.getHost());
+        config.setPort(uri.getPort());
+
+        String[] userInfo = uri.getUserInfo().split(":");
+        config.setUsername(userInfo[0]);
+        config.setPassword(RedisPassword.of(userInfo[1]));
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()                      // 🔑 REQUIRED for Upstash
+                        .commandTimeout(Duration.ofSeconds(5))
+                        .shutdownTimeout(Duration.ofSeconds(2))
+                        .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+
+    @Bean
     public RedisTemplate<String, String> redisTemplate(
-            RedisConnectionFactory factory) {
+            LettuceConnectionFactory factory) {
 
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
